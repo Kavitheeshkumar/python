c91f36064b62feddd217c20e939f39f47ec6818d
Kavitheeshkumar <kavitheeshkumar22td0687@svcet.ac.in>
2025-02-05 15:06:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+db = SQLAlchemy(app)
+CORS(app)
 
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    courses = db.Column(db.String(255), nullable=False)
 
-@app.route('/')
-def hello_world():
-    return 'KADAVULE AJITHEYY'
+@app.route('/students', methods=['POST'])
+def add_student():
+    data = request.get_json()
+    new_student = Student(name=data['name'], age=data['age'], courses=','.join(data['courses']))
+    db.session.add(new_student)
+    db.session.commit()
+    return jsonify({'message': 'Student added successfully!'}), 201
 
-# main driver function
+@app.route('/students', methods=['GET'])
+def get_students():
+    students = Student.query.all()
+    output = [{
+        'id': student.id,
+        'name': student.name,
+        'age': student.age,
+        'courses': student.courses.split(',')
+    } for student in students]
+    return jsonify(output)
+
 if __name__ == '__main__':
-
-    app.run()
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
